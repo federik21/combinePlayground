e373548231c5b7769910055afc07adff937cd20b
Federico Piccirilli <federico.piccirilli@zehus.it>
2021-09-22 14:44:13 +0200

    Initial Commit

---
+++ CombineShowcase/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CombineShowcase
+//
+//  Created by Piccirilli Federico on 9/22/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
